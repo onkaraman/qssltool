b3a26385488162ec6b53ba3a548bc69e024dbbfa
Onur Karaman <onur@areondev.de>
2016-02-23 12:29:58 +0100

    SettingsWindow: Progress: Applying selection to samples.

---
+++ QSSL Tool/Gateways/ColorSettings.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Media;
+
+namespace QSSLTool.Gateways
+{
+    public class ColorSettings
+    {
+        public Color NeutralBG;
+        public Color NeutralFG;
+        public Color PositiveBG;
+        public Color PositiveFG;
+        public Color NegativeBG;
+        public Color NegativeFG;
+
+        public ColorSettings()
+        {
+            RestoreDefaults();
+        }
+
+        public void RestoreDefaults()
+        {
+            NeutralBG = Color.FromArgb(255, 255, 235, 156);
+            NeutralFG = Color.FromArgb(255, 191, 149, 0);
+            PositiveBG = Color.FromArgb(255, 198, 239, 206);
+            PositiveFG = Color.FromArgb(255, 0, 97, 0);
+            NegativeBG = Color.FromArgb(255, 255, 199, 206);
+            NegativeFG = Color.FromArgb(255, 156, 0, 6);
+        }
+    }
+}
--- QSSL Tool/SettingsWindow.xaml.cs
+++ QSSL Tool/SettingsWindow.xaml.cs
@@ -17,8 +17,8 @@ namespace QSSLTool
             InitializeComponent();
             _colorSettings = new ColorSettings();
 
-            restoreColors();
             setupViews();
+            restoreColors();
         }
 
         private void setupViews()
@@ -45,31 +45,37 @@ namespace QSSLTool
         private void NeutralFGPickerColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
         {
             _colorSettings.NeutralFG = (Color) NeutralFGPicker.SelectedColor;
+            NeutralSampleTB.Foreground = new SolidColorBrush(_colorSettings.NeutralFG);
         }
 
         private void NeutralBGPickerColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
         {
             _colorSettings.NeutralBG = (Color)NeutralBGPicker.SelectedColor;
+            NeutralSampleTB.Background = new SolidColorBrush(_colorSettings.NeutralBG);
         }
 
         private void PositiveFGPickerColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
         {
             _colorSettings.PositiveFG = (Color)PositiveFGPicker.SelectedColor;
+            PositiveSampleTB.Foreground = new SolidColorBrush(_colorSettings.PositiveFG);
         }
 
         private void PositiveBGPickerColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
         {
             _colorSettings.PositiveBG = (Color)PositiveBGPicker.SelectedColor;
+            PositiveSampleTB.Background = new SolidColorBrush(_colorSettings.PositiveBG);
         }
 
         private void NegativeFGPickerColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
         {
             _colorSettings.NegativeFG = (Color)NegativeFGPicker.SelectedColor;
+            NegativeSampleTB.Foreground = new SolidColorBrush(_colorSettings.NegativeFG);
         }
 
         private void NegativeBGPickerColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
         {
             _colorSettings.NegativeBG = (Color)NegativeBGPicker.SelectedColor;
+            NegativeSampleTB.Background = new SolidColorBrush(_colorSettings.NegativeBG);
         }
         #endregion
     }
